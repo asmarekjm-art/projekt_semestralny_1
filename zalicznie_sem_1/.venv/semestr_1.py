6adb69b29d1aceee20ac437c031d9e1563be449a
asmarekjm-art <asmarek.jm@gmail.com>
2026-02-16 10:07:35 +0100

    filtrowanie danych

--- zalicznie_sem_1/.venv/semestr_1.py
+++ zalicznie_sem_1/.venv/semestr_1.py
@@ -20,3 +20,61 @@ def wczytaj_dane():
         pokaz(df)
     except Exception as e:
         messagebox.showerror("Błąd", str(e))
+def filtruj_dane():
+    global df
+
+    if df is None:
+        messagebox.showwarning("Brak danych", "Najpierw wczytaj plik CSV")
+        return
+
+    dane = df.copy()
+
+    # ===== PŁEĆ =====
+    plec = []
+    if var_k.get():
+        plec.append("K")
+    if var_m.get():
+        plec.append("M")
+
+    if plec:
+        dane = dane[dane["plec"].isin(plec)]
+
+    # ===== WIEK =====
+    try:
+        if entry_min.get() != "":
+            dane = dane[dane["wiek"] >= int(entry_min.get())]
+
+        if entry_max.get() != "":
+            dane = dane[dane["wiek"] <= int(entry_max.get())]
+
+    except ValueError:
+        messagebox.showwarning("Błąd", "Wiek musi być liczbą")
+        return
+
+    # ===== CUKRZYCA =====
+    cuk = []
+    if var_cuk_tak.get():
+        cuk.append("tak")
+    if var_cuk_nie.get():
+        cuk.append("nie")
+
+    if cuk:
+        dane = dane[dane["cukrzyca"].isin(cuk)]
+
+    # ===== NADCIŚNIENIE =====
+    nad = []
+    if var_nad_tak.get():
+        nad.append("tak")
+    if var_nad_nie.get():
+        nad.append("nie")
+
+    if nad:
+        dane = dane[dane["nadcisnienie"].isin(nad)]
+
+    pokaz(dane)
+
+
+# ===== GUI =====
+okno = tk.Tk()
+...
+okno.mainloop()
